f637c044fc686516e981174a3f601d7f0e91db7a
MACH <maier.christian@gmx.at>
2021-01-04 20:41:53 +0100

    ColorButtons Background changing

--- app/src/main/java/com/kro/simplemessenger/ColorPicker.java
+++ app/src/main/java/com/kro/simplemessenger/ColorPicker.java
@@ -2,8 +2,12 @@ package com.kro.simplemessenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ColorPicker extends AppCompatActivity {
 
@@ -18,7 +22,31 @@ public class ColorPicker extends AppCompatActivity {
      * @param view current view element
      */
     public void onColorPicker(View view){
+        Button myButton = (Button) findViewById(view.getId());
 
+        /**
+         * get resource by name as getting background tint is only possible from API 21+
+         * Author: theJoe7
+         * Date: 19/12/2013
+         * Title: Android : ImageView getID(); returning integer
+         * Source: https://stackoverflow.com/questions/3920833/android-imageview-getid-returning-integer
+         */
+        String myId = getResources().getResourceEntryName(myButton.getId());
+
+        /**
+         * get color by name
+         * Author: mihanovak1024
+         * Date: 02/10/2016
+         * Title: Android : Possible to get color with string?
+         * Source: https://stackoverflow.com/questions/39817299/possible-to-get-color-with-string
+         */
+        int colorId = getResources().getIdentifier(myId, "color", getPackageName());
+        Integer myColor = getResources().getColor(colorId);
+
+        String myColorName = myColor.toString();
+        Toast.makeText(this, myColorName, Toast.LENGTH_SHORT).show();
+
+        view.setBackgroundColor(myColor);
     }
 
 }
