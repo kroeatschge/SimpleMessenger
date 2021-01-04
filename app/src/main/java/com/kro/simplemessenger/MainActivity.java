dd78108aa3252ec36b01452f185eb71b1ddfa6da
MACH <maier.christian@gmx.at>
2021-01-04 14:02:57 +0100

    settings menu adjusted

--- app/src/main/java/com/kro/simplemessenger/MainActivity.java
+++ app/src/main/java/com/kro/simplemessenger/MainActivity.java
@@ -7,6 +7,11 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.View;
 
@@ -21,15 +26,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -46,8 +42,14 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            /**
+             * Source: https://stackoverflow.com/questions/53902494/navigation-component-cannot-find-navcontroller
+             * Author: JunaidKhan
+             * Date: 03/12/2019
+             * Title: Navigation Component: Cannot find NavController
+             */
+            Navigation.findNavController(this,R.id.nav_host_fragment).navigate(R.id.action_FirstFragment_to_SecondFragment);
             return true;
         }
 
